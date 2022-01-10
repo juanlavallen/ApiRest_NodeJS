71d6496b7d7ed5e560dc7cab2e57fc8280130ad6
Juan Lavallen <juanlavallen02@gmail.com>
2022-01-10 19:00:18 -0300

    validando la contraseña

--- routes/users.js
+++ routes/users.js
@@ -4,19 +4,20 @@ const { check } = require('express-validator');
 const { validateFields } = require('../middlewares/validate-fields');
 const { email } = require('../helpers/db-validators');
 
-const { 
-    userGet, 
-    userPost, 
-    userPut, 
+const {
+    userGet,
+    userPost,
+    userPut,
     userDelete } = require('../controllers/users');
-    
+
 const router = Router();
 
 router.get('/', userGet);
 
-router.post('/',[
+router.post('/', [
     check('name', 'El nombre es obligatorio').notEmpty(),
     check('email', 'El email es obligatorio').custom(email).notEmpty(),
+    check('password', 'La contraseña es obligatoria').notEmpty().isLength({ min: 8 }),
     validateFields
 ], userPost);
 
