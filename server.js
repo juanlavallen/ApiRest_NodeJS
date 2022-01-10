122df7ab8a852981e82201a07f0746b4a393d917
Juan Lavallen <juanlavallen02@gmail.com>
2022-01-10 15:45:27 -0300

    creando server express

---
+++ server.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`Server running on port: http://localhost:${PORT}`);
+});
