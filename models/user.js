f3d2b91644130a3d5371c29c581774bca661a3f0
Juan Lavallen <juanlavallen02@gmail.com>
2022-01-10 18:36:34 -0300

    creando modelo de usuario

---
+++ models/user.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        required: true,
+        enum: ['ADMIN_ROLE', 'USER_ROLE']
+    },
+    status: {
+        type: Boolean,
+        default: true
+    },
+    google: {
+        type: Boolean,
+        default: false
+    }
+});
+
+
+module.exports = model('User', UserSchema);
