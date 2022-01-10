29155e311e318292838f4e36848da275363fb255
Juan Lavallen <juanlavallen02@gmail.com>
2022-01-10 15:48:01 -0300

    creando directorio public

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const PORT = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static('public'));
+
 app.listen(PORT, () => {
     console.log(`Server running on port: http://localhost:${PORT}`);
 });
