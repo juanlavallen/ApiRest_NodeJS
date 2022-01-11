5d6ff2472bbe836dfe5918b3585ec13a42195c47
Juan Lavallen <juanlavallen02@gmail.com>
2022-01-11 12:03:23 -0300

    controller para el login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,46 @@
+const bcryptjs = require('bcryptjs');
+const { generateJWT } = require('../helpers/generate-jwt');
+const User = require('../models/user');
 
-const login = (req, res) => {
-    res.json({
-        msg: 'login ok'
-    });
+const login = async (req, res) => {
+
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(400).json({
+                msg: 'El correo es incorrecto'
+            });
+        }
+
+        if (!user.status) {
+            return res.status(400).json({
+                msg: 'El correo ya no es valido'
+            });
+        }
+
+        const validatePassword = bcryptjs.compareSync(password, user.password);
+        if (!validatePassword) {
+            return res.status(400).json({
+                msg: 'La contraseña es incorrecta'
+            })
+        }
+
+        const token = generateJWT(user.id);
+
+        res.json({
+            user,
+            token
+        });
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            msg: 'Ocurrio un error'
+        });
+    }
 }
 
 module.exports = {
